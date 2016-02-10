5a821b2f5479101b6e7f523971663623962259c8
Anthony Sciutto <sciuttam@mail.uc.edu>
2016-02-10 09:44:30 -0500

    first commit

---
+++ project1.cpp
@@ -0,0 +1,13 @@
+//Advanced Data Structures & Algorithims (EECE4040)
+//Lab I
+//Anthony Sciutto & Luke Hocker
+//2-10-2016
+//
+
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+
+
